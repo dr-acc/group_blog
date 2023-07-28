64e9d86bdf8f9a1c77cfe8692232d5d3fe95d865
Luis Herrera <donluisherrera>
2023-07-28 10:17:12 -0700

    created post comments table and schema

---
+++ lib/blog/comments/comment.ex
@@ -0,0 +1,19 @@
+defmodule Blog.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :content, :string
+    belongs_to :post, Blog.Posts.Post
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content, :post_id])
+    |> validate_required([:content, :post_id])
+    |> foreign_key_constraint(:post_id)
+  end
+end
