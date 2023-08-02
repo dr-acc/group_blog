83216730d2eb7bde0133e29dc4dcb5c5e10df80d
BLAXPIA05 <reaganw762@gmail.com>
2023-08-02 11:10:07 -0600

    Draft add post and user assoc
    Co-authored-by: Luis Herrera <Donluisherrera@gmail.com>
    Co-authored-by: Dr. Alena <dr-acc@users.noreply.github.com>

--- lib/blog/posts/post.ex
+++ lib/blog/posts/post.ex
@@ -8,6 +8,7 @@ defmodule Blog.Posts.Post do
     field :title, :string
     field :published_on, :utc_datetime
     has_many :comments, Blog.Comments.Comment
+    belongs_to :user, Blog.Accounts.User
 
     timestamps()
   end
@@ -15,7 +16,8 @@ defmodule Blog.Posts.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :visibility, :content, :published_on])
-    |> validate_required([:title, :visibility, :content])
+    |> cast(attrs, [:title, :visibility, :content, :published_on, :user_id])
+    |> validate_required([:title, :visibility, :content, :user_id])
+    |> foreign_key_constraint(:user_id)
   end
 end
